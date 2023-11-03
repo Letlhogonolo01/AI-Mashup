1a0ec6235f59c637e82de738874eafd761c33717
ThandolwenkosiTee <teeshlaine@gmail.com>
2023-11-03 22:48:38 +0200

    Screens

---
+++ Screens/HomeScreen.js
@@ -0,0 +1,14 @@
+// HomeScreen.js
+import React from 'react';
+import { View, Text } from 'react-native';
+
+function HomeScreen() {
+  return (
+    <View>
+      <Text>Welcome to the Home Screen</Text>
+      {/* Add more UI components and logic here */}
+    </View>
+  );
+}
+
+export default HomeScreen;
