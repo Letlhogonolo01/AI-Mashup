5cb9fd614c91d7880e6f5de033075c9d5c3db705
ThandolwenkosiTee <teeshlaine@gmail.com>
2023-11-04 08:48:49 +0200

    app

---
+++ APP/azureAdConfig.js
@@ -0,0 +1,7 @@
+export const azureAdConfig = {
+    clientId: 'c59abe4b-2949-4ece-bec1-1f3aae6caeff',
+    authority: 'https://login.microsoftonline.com/0a75c5ba-abb0-4278-938a-ca6fb62022d5',
+    redirectUri: 'YOUR_REDIRECT_URI',
+    scopes: ['user.read'], // Add the required scopes for your app
+  };
+  
